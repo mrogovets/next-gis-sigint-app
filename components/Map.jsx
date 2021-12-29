5e66d72b50d81d283c9d2217f2fcff75df6c7fa3
mrogovets <mrogovets@ukr.net>
2021-12-29 14:04:20 +0200

    initial from local position done

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,47 +3,48 @@ import { Loader } from "@googlemaps/js-api-loader";
 import PropTypes from "prop-types";
 function Map({ setMapObject }) {
   const googlemap = useRef(null);
-  const [lat, setLat] = useState(34.9988127);
-  const [lng, setLng] = useState(135.7674863);
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_API_KEY,
       version: "weekly",
     });
-    let map;
-    const userLocation = () => {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLat(position.coords.latitude);
-        setLng(position.coords.longitude);
-      });
-      console.log(lat, lng);
-    };
 
-    loader.load().then(() => {
-      // Setting parameters for embedding Google Maps
-      const initialView = {
-        center: {
-          lat: lat,
-          lng: lng,
-        },
-        zoom: 14,
-      };
-      const buttonsDisabled = {
-        fullscreenControl: false,
-        mapTypeControl: false,
-        streetViewControl: false,
-        zoomControl: false,
-      };
-      // Embedding Google Maps
-      const google = window.google;
-      map = new google.maps.Map(googlemap.current, {
-        ...initialView,
-        ...buttonsDisabled,
+    let map;
+
+    if (navigator.geolocation) {
+      loader.load().then(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          const initialView = {
+            center: {
+              lat: pos.lat,
+              lng: pos.lng,
+            },
+            zoom: 14,
+          };
+          const buttonsDisabled = {
+            fullscreenControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+            zoomControl: false,
+          };
+          // Embedding Google Maps
+          const google = window.google;
+          map = new google.maps.Map(googlemap.current, {
+            ...initialView,
+            ...buttonsDisabled,
+          });
+
+          setMapObject(map); // NOTE
+        });
       });
-      userLocation();
-      setMapObject(map); // NOTE
-    });
-  }, [setMapObject]);
+    } else {
+      console.log("None navigator");
+    }
+  }, []);
 
   return (
     <React.Fragment>
