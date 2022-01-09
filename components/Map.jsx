47d5cb54e776c63877dbacf931b3061cd2609bad
mrogovets <mrogovets@ukr.net>
2022-01-09 20:52:19 +0200

    added svg marker 090122

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
-
+import { faBus } from "@fortawesome/free-solid-svg-icons";
 function Map({ setMapObject }) {
   const googlemap = useRef(null);
   useEffect(() => {
@@ -35,9 +35,33 @@ function Map({ setMapObject }) {
           // Embedding Google Maps
           const google = window.google;
 
+          const svgMarker = {
+            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+            fillColor: "blue",
+            fillOpacity: 0.6,
+            strokeWeight: 0,
+            rotation: 0,
+            scale: 2,
+            anchor: new google.maps.Point(15, 30),
+          };
+
           const marker = new google.maps.Marker({
             position: pos,
+            icon: {
+              path: faBus.icon[4],
+              fillColor: "#0000ff",
+              fillOpacity: 1,
+              anchor: new google.maps.Point(
+                faBus.icon[0] / 2, // width
+                faBus.icon[1] // height
+              ),
+              strokeWeight: 1,
+              strokeColor: "#ffffff",
+              scale: 0.075,
+            },
             map: map,
+            label: "This is your position",
+            title: "You are there",
           });
 
           map = new google.maps.Map(googlemap.current, {
