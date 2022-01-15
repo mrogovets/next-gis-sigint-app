651afe32eb9323829ea15669b87c870e9ac8a654
mrogovets <mrogovets@ukr.net>
2022-01-15 20:59:42 +0200

    i can add marker on click

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import { MapComponent } from "../components/MapComponent";
-import { Marker } from "../components/Marker";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 function HomePage() {
   // const [mapObject, setMapObject] = useState(null); // this is fro LocatorButton component
 
   const [lat, setLat] = useState(0);
   const [lng, setLng] = useState(0);
 
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-
   const center = { lat, lng };
   const zoom = 10;
+
+  const [clickLatLng, setClickLatLng] = useState(center);
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  const containerStyle = {
+    width: "1200px",
+    height: "800px",
+  };
 
   const getUserLocation = () => {
     if (navigator.geolocation) {
@@ -32,13 +37,27 @@ function HomePage() {
     getUserLocation();
   }, []);
 
+  const onLoad = (marker) => {
+    console.log("marker: ", marker);
+  };
+
+  const onMapClick = (mapsMouseEvent) => {
+    const clickLatLngTmp = mapsMouseEvent.latLng.toJSON();
+    setClickLatLng(clickLatLngTmp);
+  };
+
   return (
     <React.Fragment>
-      <Wrapper apiKey={apiKey}>
-        <MapComponent center={center} zoom={zoom}>
-          <Marker position={center} />
-        </MapComponent>
-      </Wrapper>
+      <LoadScript googleMapsApiKey={apiKey}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={zoom}
+          onClick={onMapClick}>
+          <Marker onLoad={onLoad} position={center} />
+          <Marker position={clickLatLng} />
+        </GoogleMap>
+      </LoadScript>
     </React.Fragment>
   );
 }
