9d5845c9a5a6e22c300bcbac177c2bb31f3dee8b
mrogovets <mrogovets@ukr.net>
2022-03-30 08:02:17 +0300

    realized authorization in firebase

--- components/cloudFirestore/write.jsx
+++ components/cloudFirestore/write.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import firebase from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const WriteCloudFirestore = () => {
   const sendData = () => {
-    try {
-      const firestore = getFirestore();
-      console.log(firestore);
-      // .collection("myCollection")
-      // .doc("my_document")
-      // .set({
-      //   string_data: "Charlie",
-      //   number: 1979,
-      //   array_data: ["GisSigint", 2021],
-      //   time_stamp: firebase.firestore.TimeStamp.fromDate(new Date.now()),
-      // })
-      // .then(alert("Data was successfully sent to cloudstore!"));
-    } catch (error) {
-      console.log(error);
-      alert(error);
-    }
+    console.log("sendData");
   };
-
   return (
     <React.Fragment>
       <button onClick={sendData}>Send Data to Cloud Firestore</button>
---
+++ pages/auth.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "../firebase/firebaseConfig";
+
+// Configure FirebaseUI
+const uiConfig = {
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function SignInScreen() {
+  return (
+    <div
+      style={{
+        maxWidth: "320px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      <h1>GisSigInt Login</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+export default SignInScreen;
