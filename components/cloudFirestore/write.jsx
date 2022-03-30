6be7da3b9af08e9d902a146f8da3ee6b5653686f
mrogovets <mrogovets@ukr.net>
2022-03-30 19:46:59 +0300

    write info in Cloud Firestore

--- components/cloudFirestore/write.jsx
+++ components/cloudFirestore/write.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 
+import firebase from "../../firebase/firebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+
 const WriteCloudFirestore = () => {
+  // Firestore
+  const db = firebase.firestore();
+
+  const [user, loading, error] = useAuthState(firebase.auth());
+  console.log("Loading:", loading, "|", "Current user: ", user);
+
+  const [votes, votesLoading, votesError] = useCollection(
+    firebase.firestore().collection("votes"),
+    {}
+  );
+
+  if (!votesLoading && votes) {
+    votes.docs.map((doc) => console.log(doc.data()));
+  }
+
+  // create document function
+  const addVoteDocument = async (vote) => {
+    await db.collection("votes").doc(user.uid).set({
+      vote,
+    });
+  };
+
   const sendData = () => {
-    console.log("sendData");
+    // console.log("sendData");
+    addVoteDocument("hello");
   };
   return (
     <React.Fragment>
