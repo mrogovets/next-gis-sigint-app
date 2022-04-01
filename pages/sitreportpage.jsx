642ee8ca24b4d974be47d2c29f9f2317c8a29840
mrogovets <mrogovets@ukr.net>
2022-04-02 02:40:26 +0300

    created SitRepPage

---
+++ pages/sitreportpage.jsx
@@ -0,0 +1,68 @@
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Head from "next/head";
+
+function SitRepPage() {
+  const onClickHandler = () => {
+    console.log("click in sitreportpage.jsx");
+  };
+  return (
+    <React.Fragment>
+      <Head>
+        <title>GIS SigInt | SITREPORT</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:title"
+          content="GIS SigInt System"
+          key="GIS, SigInt, System"
+        />
+      </Head>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={onClickHandler}>
+              <MenuIcon />
+            </IconButton>
+            <Link href={"/"}>
+              <Avatar
+                sx={{
+                  width: 60,
+                  height: 60,
+                }}>
+                <Image src="/gerbTCSigint.png" alt="emblem" layout="fill" />
+              </Avatar>
+            </Link>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, textAlign: "center" }}>
+              GIS SigInt System
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ marginTop: "64px" }}>
+        <h1>Situation Report Page</h1>
+      </Box>
+    </React.Fragment>
+  );
+}
+export default SitRepPage;
