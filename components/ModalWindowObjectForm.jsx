3d49a6d9f9481b4f705fac1e9e34b8901e67dd61
mrogovets <mrogovets@ukr.net>
2022-05-27 01:00:54 +0300

    Edited Modal Window for SigIntObj

--- components/ModalWindowObjectForm.jsx
+++ components/ModalWindowObjectForm.jsx
@@ -1,6 +1,12 @@
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { TextField } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 
 const style = {
   position: "absolute",
@@ -8,17 +14,29 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
-  height: 400,
+  height: 440,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  "& .MuiTextField-root": { m: 0.7, width: "100%" },
 };
 
 export default function BasicModal({
   openModalWindowObject,
   closeModalWindowObject,
 }) {
+  const [value, setValue] = useState("Controlled");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const timeElapsed = Date.now();
+  const [valueDate, setValueDate] = useState(new Date(timeElapsed));
+
+  const handleChangeDate = (newValueDate) => {
+    setValueDate(newValueDate);
+  };
   return (
     <div>
       <Modal
@@ -26,13 +44,45 @@ export default function BasicModal({
         onClose={closeModalWindowObject}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description">
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+        <Box sx={style} noValidate autoComplete="off">
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ textAlign: "center" }}>
+            Формуляр об`єкта розвідки
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <TextField
+            id="outlined-textarea"
+            label="Найменування об'єкта розвідки"
+            placeholder="Введіть найменування об'єкта розвідки"
+            multiline
+          />
+          <TextField
+            id="outlined-textarea"
+            label="Місцезнаходження об'єкта розвідки"
+            placeholder="широта | довгота"
+            value={"широта: | довгота:"}
+            onChange={handleChange}
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="В яких джерела проявляється"
+            placeholder="Введіть джерела, в яких проявляється об'єкт розвідки"
+            multiline
+            rows={4}
+          />
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Stack spacing={3}>
+              <MobileDatePicker
+                label="Дата останнього підтвердження"
+                inputFormat="dd.MM.yyyy"
+                value={valueDate}
+                onChange={handleChangeDate}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </Stack>
+          </LocalizationProvider>
         </Box>
       </Modal>
     </div>
