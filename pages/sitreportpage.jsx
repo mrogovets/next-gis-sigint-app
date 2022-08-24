e1b8e1ba4b5fd498dcb65c410de5e894e06a2f43
mrogovets <mrogovets@ukr.net>
2022-08-24 21:42:29 +0300

    created Loader.jsx

--- pages/sitreportpage.jsx
+++ pages/sitreportpage.jsx
@@ -8,11 +8,14 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 import MenuIcon from "@mui/icons-material/Menu";
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 import { ReadFromCloudFirestore } from "../firebase/readFirestore";
+import Loader from "../components/Loader";
 
 function SitRepPage() {
   const onClickHandler = () => {
@@ -28,55 +31,77 @@ function SitRepPage() {
       setFirestoreData(fromFirestoreData);
     }
   }, [fromFirestoreData]);
+
   //-------- \Read situation from DB --------------------
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>GIS SigInt | SITREPORT</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="og:title"
-          content="GIS SigInt System"
-          key="GIS, SigInt, System"
-        />
-      </Head>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={onClickHandler}>
-              <MenuIcon />
-            </IconButton>
-            <Link href={"/"}>
-              <Avatar
-                sx={{
-                  width: 60,
-                  height: 60,
-                }}>
-                <Image src="/gerbTCSigint.png" alt="emblem" layout="fill" />
-              </Avatar>
-            </Link>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, textAlign: "center" }}>
-              GIS SigInt System
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Box sx={{ marginTop: "64px" }}>
-        <h1>Situation Report Page</h1>
-        {JSON.stringify(firestoreData)}
-      </Box>
-    </React.Fragment>
-  );
+  if (firestoreData) {
+    const dataReport = new Date(
+      firestoreData.time_stamp.seconds
+    ).toLocaleString("en-GB", { timeZone: "UTC" });
+    return (
+      <React.Fragment>
+        <Head>
+          <title>GIS SigInt | SITREPORT</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            property="og:title"
+            content="GIS SigInt System"
+            key="GIS, SigInt, System"
+          />
+        </Head>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="fixed">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={onClickHandler}>
+                <MenuIcon />
+              </IconButton>
+              <Link href={"/"}>
+                <Avatar
+                  sx={{
+                    width: 60,
+                    height: 60,
+                  }}>
+                  <Image src="/gerbTCSigint.png" alt="emblem" layout="fill" />
+                </Avatar>
+              </Link>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, textAlign: "center" }}>
+                GIS SigInt System
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Box sx={{ marginTop: "64px" }}>
+          <CssBaseline />
+          <Container fixed>
+            <Box sx={{ bgcolor: "#cfe8fc", height: "100vh", width: "100%" }}>
+              <h1>Situation Report Page</h1>
+              <h1>Розвідувальне зведення станом на {dataReport}</h1>
+              {JSON.stringify(firestoreData)}
+            </Box>
+          </Container>
+        </Box>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h1>LOADING...</h1>
+        <Loader />
+      </React.Fragment>
+    );
+  }
 }
 export default SitRepPage;
